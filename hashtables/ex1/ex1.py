948b745c432b1ee2600e316674e75ac5b88da81c
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2020-02-10 23:30:42 -0500

    added answer variable added loop to insert weight values into hash table

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,13 +7,24 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    ht = HashTable(length)
 
     """
     YOUR CODE HERE
     """
+    answer = []
 
-    return None
+    for weight in weights:
+        # if hash_table_retrieve is None
+        if hash_table_retrieve(ht, weight) is None:
+            hash_table_insert(ht, weight, (limit-weight))
+
+    print(ht.storage.items())
+
+    # loop through ht to compare keys to value to find the pair that equal the limit
+    # for index in ht.storage:
+    #     print(ht.storage[index])
+    # return None
 
 
 def print_answer(answer):
@@ -21,3 +32,9 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+w = [ 4, 6, 10, 15, 16 ]
+length = 5
+limit = 21
+
+get_indices_of_item_weights(w, length, limit)
